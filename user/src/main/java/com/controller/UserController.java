d51b0342f501998c46ff5c837fa26c995f0ded7e
qiushuzhao <1284177328@qq.com>
2017-04-24 17:53:49 +0800

    all

--- user/src/main/java/com/controller/UserController.java
+++ user/src/main/java/com/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.service.UserService;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping("/main")
 	public String view(){
 		return "main";
 	}
+	
+	
 }
--- user/src/main/webapp/WEB-INF/jsp/main.jsp
+++ user/src/main/webapp/WEB-INF/jsp/main.jsp
@@ -4,9 +4,15 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery-2.0.3.min.js"></script>
 <title>Insert title here</title>
 </head>
 <body>
 	main.jsp
 </body>
+<script type="text/javascript">
+	$(function(){
+		
+	});
+</script>
 </html>
